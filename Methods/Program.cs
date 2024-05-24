cab7e39ea2e5396784c943ff8a5c6864b40a1c0f
Twila [Hyper] Habab <twila.shayne.habab@outlook.com>
2024-05-24 23:34:07 +0100

    Reference Parameters

--- Methods/Program.cs
+++ Methods/Program.cs
@@ -67,6 +67,7 @@ namespace Methods
             Optional.ShowOptional();
             Named.ShowNamed();
             OutParameters.ShowOutParameters();
+            ReferenceParameters.ShowReferenceParameters();
 
             Console.ReadLine();
         }
---
+++ Methods/ReferenceParameters.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods
+{
+    public static class ReferenceParameters
+    {
+        public static void ShowReferenceParameters()
+        {
+            int num = 10;
+            string name = "Hyper";
+            Assign(ref num, ref name);
+
+            Console.WriteLine(num + name);
+
+            string newName = Console.ReadLine();
+            if (ChangeName(ref name, newName))
+            {
+                Console.WriteLine($"Your new name is {name}!");
+
+            }
+            else
+            {
+                Console.WriteLine("New name cannot be empty or null!");
+            }
+
+        }
+
+        // Pass by reference
+        // If using out, you have to use the value
+        static void Assign(ref int num, ref string name)
+        {
+            name = "Max";
+            num = 20;
+        }
+
+        static bool ChangeName(ref string name, string newName)
+        {
+            if (string.IsNullOrEmpty(newName))
+            {
+                name = newName;
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
